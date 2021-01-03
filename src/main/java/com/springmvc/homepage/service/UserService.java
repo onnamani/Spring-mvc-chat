1392915ef89de262817caa17821398d1f07e6108
Obinna <>
2021-01-03 15:37:16 +0100

    created AuthenticationService.java, HashService.java and UserService.java components

--- src/main/java/com/springmvc/homepage/service/UserService.java
+++ src/main/java/com/springmvc/homepage/service/UserService.java
@@ -4,8 +4,40 @@ import com.springmvc.homepage.POJO.User;
 import com.springmvc.homepage.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @Service
 public class UserService {
 
-    private UserMapper userMapper;
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
+    public Integer createUser(User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insertUser(new User(null,
+                user.getUsername(),
+                user.getSalt(),
+                user.getPassword(),
+                user.getFirstname(),
+                user.getLastname()
+        ));
+    }
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
+    }
 }
